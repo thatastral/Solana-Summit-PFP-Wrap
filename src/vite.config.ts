8b4b2d312b9635de5557e8b0742d7cca7418ea78
Astral <168305149+thatastral@users.noreply.github.com>
2025-10-28 08:30:10 +0100

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from 'path';
 import { figmaAssetsPlugin } from './vite-plugin-figma-assets';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), figmaAssetsPlugin()],
