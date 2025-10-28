506f1c67b8125f968e8bdb6781cee9c1723e6c0a
Astral <168305149+thatastral@users.noreply.github.com>
2025-10-28 08:29:10 +0100

    Update vite-plugin-figma-assets.ts

--- src/vite-plugin-figma-assets.ts
+++ src/vite-plugin-figma-assets.ts
@@ -1,6 +1,6 @@
 import type { Plugin } from 'vite';
-import fs from 'node:fs';
-import path from 'node:path';
+import fs from 'fs';
+import path from 'path';
 
 /**
  * Vite plugin to handle figma:asset imports
